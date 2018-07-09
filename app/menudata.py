048b23713a08f265a6a7516eb5bed3df1ce8e5b0
mattpodolak <mpodola2@uwo.ca>
2018-07-09 03:36:00 -0400

    Changed navbar and styling

--- app/menudata.py
+++ app/menudata.py
@@ -1,10 +1,21 @@
-class MenuItems:
-    def __init__(self, name, desc, category, display_price):
-        self.name = name
-        # self.id = random hash based off of name and desc
-        self.desc = desc
-        self.category = category
-        self.display_price = display_price
+deal_items = [
+        {
+            'name': 'Large Pizza Deal',
+            'desc': 'Beautiful day in Portland!',
+            'price': '15'
+        },
+        {
+            'name': 'Large Pizza Deal',
+            'desc': 'Beautiful day in Portland!',
+            'price': '15'
+        },
+        {
+            'name': 'Large Pizza Deal',
+            'desc': 'Beautiful day in Portland!',
+            'price': '15'
+        }
+    ]
 
-#pizza = MenuItems('Large Pepperoni Pizza', 'A large fresh and delicious Brooklyn style pizza', 'Deals', )
+ menu_items = {'deals': deal_items}
+
 
