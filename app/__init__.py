76ce6a7fda37184f6bb9bae4b51a3458603d91e1
mattpodolak <mpodola2@uwo.ca>
2018-07-17 15:06:13 -0400

    Added logout functionality

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,7 @@ db = SQLAlchemy(flapp)
 migrate = Migrate(flapp, db)
 login = LoginManager(flapp)
 
+login = LoginManager(app)
+login.login_view = 'login'
+
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,9 @@ from app import flapp
 from app.forms import LoginForm
 from flask import render_template, flash, redirect
 from app.menudata import menu_items
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.models import User
+from werkzeug.urls import url_parse
 
 @flapp.route('/')
 @flapp.route('/index')
@@ -45,7 +46,7 @@ def login():
     # check if logged in currently
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-        
+
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -54,5 +55,13 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
