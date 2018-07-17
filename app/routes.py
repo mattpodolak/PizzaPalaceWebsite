656dcd2778250dbd3de10383be10613fffc8fb14
mattpodolak <mpodola2@uwo.ca>
2018-07-17 15:55:53 -0400

    add registration

--- app/routes.py
+++ app/routes.py
@@ -66,6 +66,16 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@flapp.route('/signup')
-def signup():
-    return redirect(url_for('index'))
+@flapp.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
