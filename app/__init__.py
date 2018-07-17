d58e8db32c92847a7abef7b312c5d23302fa5f6c
mattpodolak <mpodola2@uwo.ca>
2018-07-17 14:40:33 -0400

    incorporating login

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 flapp = Flask(__name__)
 flapp.config.from_object(Config)
 db = SQLAlchemy(flapp)
 migrate = Migrate(flapp, db)
+login = LoginManager(flapp)
 
 from app import routes, models
